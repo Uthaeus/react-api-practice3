1dddeb4c0ddd2062316154bc88eac4e9acada2a0
Uthaeus <romanlavery@gmail.com>
2023-05-13 18:50:14 -0600

    added initial styling for homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -32,24 +32,24 @@ function HomePage() {
     console.log(data);
 
     return (
-        <div>
+        <div className="home-container">
             <h1>Home Page</h1>
             <hr />
             {isLoading && <p>Loading...</p>}
             {!isLoading && (
-                <div>
-                    <div>
-                        <h2>Posts</h2>
+                <div className="home-content-wrapper">
+                    <div className="home-content">
+                        <h2 className="home-content-title">Posts</h2>
                         {data.posts?.map(post => <PostItem key={post.id} post={post} />)}
                     </div>
 
-                    <div>
-                        <h2>Meetups</h2>
+                    <div className="home-content">
+                        <h2 className="home-content-title">Meetups</h2>
                         {data.meetups?.map(meetup => <MeetupItem key={meetup.id} meetup={meetup} />)}
                     </div>
 
-                    <div>
-                        <h2>Products</h2>
+                    <div className="home-content">
+                        <h2 className="home-content-title">Products</h2>
                         {data.products?.map(product => <ProductItem key={product.id} product={product} />)}
                     </div>
                 </div>
